195d84c21234de44c9c4ec8211702329e9d49bf2
Raymond Robinson <rayissach@raymonds-mbp.attlocal.net>
2018-05-10 01:39:11 -0700

    modified project page content

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -11,61 +11,63 @@ class Projects extends React.Component {
                 <div className='projectFiles'>
                     <div className='projectImages'>
                         <div className='projectBottomImg'>
-                            {/* <header className='postHead'> */}
                             <img className='portImages' id='portImg1' src='./port-img1.jpg' />
-                            <p>This is project #1 this project is made with react</p>
-                            {/* </header> */}
+                                <header className='postHead'>
+                                    <p>This is project #1 this project is made with react</p>
+                                </header>
                         </div>
                         <div className='projectBottomImg'>
-                            {/* <header className='postHead'> */}
                             <img className='portImages' id='portImg2' src='./port-img2.jpg' />
-                            <p>This is project #1 this project is made with react</p>
-                            {/* </header> */}
+                                <header className='postHead'>
+                                    <p>This is project #1 this project is made with react</p>
+                                </header>
                         </div>
                         <div className='projectBottomImg'>
-                            {/* <header className='postHead'> */}
                             <img className='portImages' id='portImg3' src='./port-img3.jpg' />
-                            <p>This is project #1 this project is made with react</p>
-                            {/* </header> */}
+                                <header className='postHead'>
+                                    <p>This is project #1 this project is made with react</p>
+                                </header>
                         </div>
                         <div className='projectBottomImg'>
-                            {/* <header className='postHead'> */}
                             <img className='portImages' id='portImg4' src='./port-img4.jpg' />
-                            <p>This is project #1 this project is made with react</p>
-                            {/* </header> */}
+                                <header className='postHead'>
+                                    <p>This is project #1 this project is made with react</p>
+                                </header>
                         </div>
-                        <div className='projectBottomImg'>
-                            {/* <header className='postHead'> */}
+                        <div className='projectBottomImg'> 
                             <img className='portImages' id='portImg5' src='./port-img5.jpg' />
-                            <p>This is project #1 this project is made with react</p>
-                            {/* </header> */}
+                                <header className='postHead'>
+                                    <p>This is project #1 this project is made with react</p>
+                                </header>
                         </div>
                         <div className='projectBottomImg'>
-                            {/* <header className='postHead'> */}
                             <img className='portImages' id='portImg6' src='./port-img6.jpg' />
-                            <p>This is project #1 this project is made with react</p>
-                            {/* </header> */}
+                            <header className='postHead'>
+                                <p>This is project #1 this project is made with react</p>
+                            </header>
                         </div>
                         <div className='projectBottomImg'>
-                            {/* <header className='postHead'> */}
                             <img className='portImages' id='portImg7' src='./port-img7.jpg' />
-                            <p>This is project #1 this project is made with react</p>
-                            {/* </header> */}
+                                <header className='postHead'>
+                                    <p>This is project #1 this project is made with react</p>
+                                </header>
                         </div>
                         <div className='projectBottomImg'>
-                            {/* <header className='postHead'> */}
                             <img className='portImages' id='portImg8' src='./port-img8.jpg' />
-                            <p>This is project #1 this project is made with react</p>
-                            {/* </header> */}
+                                <header className='postHead'>
+                                    <p>This is project #1 this project is made with react</p>
+                                </header>
                         </div>
                     </div>
                 </div>
                 <div className='projectFooter'>
-                    <p> These are the all of my projects from most important to least important.</p>
-                    <p>Projects show stationary picture of project. Then on hover the projects show a gif of the file along with text about the project with a trasnsparent background.</p> 
-                    <p>This then takes you to a github page when clicked</p>
+                    <div className='projectFooterText'>
+                        <p> These are the all of my projects from most important to least important.</p>
+                        <p>Projects show stationary picture of project. Then on hover the projects show a gif of the file along with text about the project with a trasnsparent background.</p> 
+                        <p>This then takes you to a github page when clicked</p>
+                    </div>
                 </div>
-            </div>
+        </div>
         )
     }
 
